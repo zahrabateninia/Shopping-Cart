3cc950031d94769a7c67c5dde987ca6931b874df
zahrabateninia <zahrabatenin@gmail.com>
2025-09-21 23:32:51 +0330

    Make login page to support both Email/Password login and Google login

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,26 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState= {
-    isLoggedIn: false,
-    user: null
-}
+const initialState = {
+  isLoggedIn: false,
+  user: null,
+};
 
 const authSlice = createSlice({
-    name: "auth",
-    initialState,
-    reducers: {
-        login:(state, action) => {
-            state.isLoggedIn = true
-            state.user = action.payload
-        },
-        logout:(state) => {
-            state.isLoggedIn = false
-            state.user = null
-        }
-    }
+  name: "auth",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      state.isLoggedIn = !!action.payload;
+      state.user = action.payload;
+    },
+    logout: (state) => {
+      state.isLoggedIn = false;
+      state.user = null;
+    },
+  },
+});
 
-})
-
-export const {login, logout} = authSlice.actions
-export default authSlice.reducer
-export const selectIsLoggedIn = (state) => state.auth.isLoggedIn
+export const { setUser, logout } = authSlice.actions;
+export default authSlice.reducer;
+export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
