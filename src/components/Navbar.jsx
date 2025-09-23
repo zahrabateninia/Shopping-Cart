4e70e0966c414569a2dbcab3a006c5b63a28faff
zahrabateninia <zahrabatenin@gmail.com>
2025-09-23 19:56:43 +0330

    Display logout if the user is logged in and login if not loggedin in the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,19 @@
-import { Link } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import { selectCartTotalQuantity } from '@/features/cart/cartSlice'
-import React from 'react'
-
+import { Link, useNavigate } from "react-router-dom"
+import { useSelector, useDispatch } from "react-redux"
+import { selectCartTotalQuantity } from "@/features/cart/cartSlice"
+import { logout } from "@/features/auth/authSlice"
+import React from "react"
 
 const Navbar = () => {
   const totalItems = useSelector(selectCartTotalQuantity)
+  const { isLoggedIn, user } = useSelector((state) => state.auth)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    dispatch(logout())
+    navigate("/") // redirect to home after logout
+  }
 
   return (
     <nav
@@ -15,13 +23,29 @@ const Navbar = () => {
         p-4 bg-gray-400 text-black shadow
       "
     >
-      <h1>logo</h1>
+      <h1 className="font-bold">Logo</h1>
+
       <div className="flex gap-4">
         <Link to="/">Home</Link>
         <Link to="/shop">Shop</Link>
         <Link to="/cart">Cart ({totalItems})</Link>
       </div>
-      <Link to="/login">Login</Link>
+
+      {isLoggedIn ? (
+        <button
+          onClick={handleLogout}
+          className="px-3 py-1 rounded bg-blue-500 text-white hover:bg-indigo-700"
+        >
+          Logout {user?.username && `(${user.username})`}
+        </button>
+      ) : (
+        <Link
+          to="/login"
+          className="px-3 py-1 rounded bg-amber-700 text-black hover:bg-indigo-700"
+        >
+          Login
+        </Link>
+      )}
     </nav>
   )
 }
