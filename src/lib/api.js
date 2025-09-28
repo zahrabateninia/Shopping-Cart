cb8a838bf159b7b597300850d0a1dfa6f643d3d0
zahrabateninia <zahrabatenin@gmail.com>
2025-09-28 06:41:42 +0330

    Fetch Fake Store API data in api file

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,24 @@
+const BASE_URL = "https://fakestoreapi.com/products";
+
+
 export async function fetchProducts() {
-    const res = await fetch('https://api.escuelajs.co/api/v1/products?offset=0&limit=30')
-    if (!res.ok) throw new Error('Failed to fetch products')
-    return res.json()
-  }
-  
+  const res = await fetch(BASE_URL);
+  if (!res.ok) throw new Error("Failed to fetch products");
+  return res.json();
+}
+
+/**
+ * @param {number} id
+ */
+export async function fetchProductById(id) {
+  const res = await fetch(`${BASE_URL}/${id}`);
+  if (!res.ok) throw new Error(`Failed to fetch product with id ${id}`);
+  return res.json();
+}
+
+
+export async function fetchProductsByCategory(category) {
+  const res = await fetch(`${BASE_URL}/category/${category}`);
+  if (!res.ok) throw new Error(`Failed to fetch products in ${category}`);
+  return res.json();
+}
